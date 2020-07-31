1eb0e086ebafb7337d2c4d3ae5f0555dc73fc261
Przemek Bisiński <38438763+pbisinski@users.noreply.github.com>
2020-07-31 21:34:11 +0200

    [ODL-1] initial project (#1)
    * [ODL-1] initial project
    
    * project structure
    * initial dependencies
    
    * update project style
    
    * added rx dependencies

---
+++ app/src/main/java/git/pbisinski/odloty/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package git.pbisinski.odloty.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import git.pbisinski.odloty.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+}
