8aeb5d0b92bff9d3391539221b9eb964e1dcc6cf
kk108 <kk10828>
2020-09-06 23:23:55 +0200

    Colors, dark-light styles added

--- app/src/main/java/git/pbisinski/odloty/view/MainActivity.kt
+++ app/src/main/java/git/pbisinski/odloty/view/MainActivity.kt
@@ -1,14 +1,26 @@
 package git.pbisinski.odloty.view
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import git.pbisinski.odloty.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    darkmode_switch.setOnClickListener {
+      when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+        Configuration.UI_MODE_NIGHT_YES ->
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        Configuration.UI_MODE_NIGHT_NO ->
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+      }
+    }
   }
 
 }
